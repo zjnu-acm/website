096516a09afcf75d1494112fb065e8084d63df87
stkevintan <stkevintan@foxmail.com>
2016-04-09 00:53:01 +0800

    express history fallback

--- server.js
+++ server.js
@@ -3,14 +3,13 @@
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
-const webpackDevServer = require('webpack-dev-server');
 var app = new (require('express'))();
 let webpackConfig = require('./webpack.config');
 const fs = require('fs-extra');
 const PORT = '8080';
 const HOST = '0.0.0.0';
+const fallback= require('express-history-api-fallback');
 const compile = process.argv.findIndex((a) => a.toLowerCase() === '-c') !== -1;
-const debug = process.argv.findIndex((a) => a.toLowerCase() === '-d') !== -1;
 fs.copySync('src/index.html','dist/index.html');
 if(compile){
     //generate static files to './dist'
@@ -20,40 +19,20 @@ if(compile){
     webpack(webpackConfig).run((err,stats)=>{
         console.log('[webpack compiled]',err?err:stats);
     });
-}else if(debug){
-    //start webpack dev server
-    webpackConfig.entry.app.unshift('webpack-dev-server/client?http://localhost:8080/','webpack/hot/dev-server');
-    webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
-    const compiler  = webpack(webpackConfig);
-
-    app.use(webpackDevMiddleware(compiler,{
-        hot:true,
-        historyApiFallback:true,
-        //noInfo:true,
-        contentBase:'./dist',
-        publicPath:'/assets/'
-    }));
-    app.use(webpackHotMiddleware(compiler));
-
-    app.get('/',(req,res)=>{
-        res.sendFile(__dirname+'/dist/index.html');
-    })
-    app.listen(PORT,HOST,function(){
-        console.log(`server is running at:${HOST}:${PORT}`);
-    });
 }else{
     //start webpack dev server
-    webpackConfig.entry.app.unshift('webpack-dev-server/client?http://localhost:8080/','webpack/hot/dev-server');
+    webpackConfig.entry.app.unshift('webpack-dev-server/client?http://localhost:8080/', 'webpack/hot/dev-server');
     webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
-    const compiler  = webpack(webpackConfig);
-    const server = new webpackDevServer(compiler,{
-        hot:true,
-        historyApiFallback:true,
+    const compiler = webpack(webpackConfig);
+
+    app.use(webpackDevMiddleware(compiler, {
         //noInfo:true,
-        contentBase:'./dist',
-        publicPath:'/assets/'
-    });
-    server.listen(PORT,HOST,function(){
+        contentBase: './dist',
+        publicPath: '/assets/'
+    }));
+    app.use(webpackHotMiddleware(compiler));
+    app.use(fallback(__dirname + '/dist/index.html'));
+    app.listen(PORT,HOST,function(){
         console.log(`server is running at:${HOST}:${PORT}`);
     });
 }
